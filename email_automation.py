b45bd35e31dd019f2a3d67a67a27c5524a325046
Anirudh Sharma <vanirudhsharma@gmail.com>
2024-09-28 20:05:19 +0530

    "Updated cohere_api_key assignment to use json.loads"

--- email_automation.py
+++ email_automation.py
@@ -19,7 +19,8 @@ from cohere import ClassifyExample
 # load_dotenv()
 
 # Initialize Cohere client
-cohere_api_key = os.environ['CO_API_KEY']  # Ensure you have COHERE_API_KEY in your environment variables
+cohere_api_key = json.loads(os.environ['CO_API_KEY'])  # Ensure you have COHERE_API_KEY in your environment variables
+
 co = cohere.Client(cohere_api_key)
 
 # Scopes for Gmail API
